fb140707dab8f0ee11ed04a8218131b2e9272bf5
masterbdx <masterbdxteam@gmail.com>
2019-11-21 22:45:53 +0200

    i added runtime file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +110,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
