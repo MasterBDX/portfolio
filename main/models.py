99ffe078aaf59cc221c96227c4a59543aed3d347
masterbdx <masterbdxteam@gmail.com>
2020-02-07 13:20:03 +0200

    ordered skills by order field

--- main/models.py
+++ main/models.py
@@ -17,5 +17,8 @@ class Skill(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
         return self.title
